0ef015e75639b672d94e67f8c302835bb9b23441
Blanco237 <asongrandy9@gmail.com>
2023-02-04 17:15:58 +0100

    final w

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Footer from './components/shared/Footer/Footer';
 import Header from './components/shared/Header/Header';
+import { UserProvider } from './Context/UserContext';
 import Ask from './pages/Ask';
 import Home from './pages/Home';
 
@@ -9,14 +10,16 @@ function App() {
 
   return (
 
-      <Router>
+    <Router>
+      <UserProvider >
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/ask' element={<Ask />}  />
+          <Route path='/ask' element={<Ask />} />
         </Routes>
         <Footer />
-      </Router>
+      </UserProvider>
+    </Router>
   );
 }
 
